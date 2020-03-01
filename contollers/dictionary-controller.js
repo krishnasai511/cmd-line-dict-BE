8fd140ab1b3ec2acbd1e75488a88dbba78a57ce1
krishnasai511 <krishnasai511@gmail.com>
2020-03-01 15:46:00 +0530

    dictionary game functionality added

--- contollers/dictionary-controller.js
+++ contollers/dictionary-controller.js
@@ -1,27 +1,35 @@
 const dict_hlpr = require('../helpers/dictionary-helper');
+const readline = require('readline');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 
 let self = module.exports = {
 
 
     //definitions
-    defnOrexmple: async (type, word, exit) => {
+    defnOrexmple: async (type, word, exit, print) => {
         try {
             let wd_defns = await dict_hlpr.defnOrexmple(type, word);
+            let wd_data = [];
             if (wd_defns['error']) {
                 console.log(`No ${type} available`);
+                return [];
             }
             else {
                 if (type == 'examples') wd_defns = wd_defns.examples;
-                console.log(`${type} are: ` + '\n');
                 wd_defns.map((wd, i) => {
-                    console.log(i + 1 + '. ' + wd.text + '\n');
+                    i == 0 && print ? console.log(`${type} are: ` + '\n') : '';
+                    if (print) {
+                        console.log(i + 1 + '. ' + wd.text + '\n');
+                    }
+                    wd_data.push(wd.text);
                 })
                 console.log(`\n`);
-            }
-            if (exit) {
-                process.exit();
+                return wd_data;
             }
         } catch (err) {
             console.log("Error ", err);
@@ -34,23 +42,29 @@ let self = module.exports = {
 
 
     //synonyms
-    synOrant: async (type, word, exit) => {
+    synOrant: async (type, word, exit, print) => {
         try {
             let data = await dict_hlpr.synOrant(word);
+            let wd_data = [];
             if (data.length) {
                 data.map(item => {
                     if (item.relationshipType == type) {
                         if (item.words.length) {
-                            console.log(`${type}s are:\n`);
-                            item.words.map(wd => console.log(wd));
-                            console.log(`\n`);
+                            wd_data = item.words;
+                            if (print) {
+                                console.log(`${type}s are:\n`);
+                                item.words.map(wd => console.log(wd));
+                                console.log(`\n`);
+                            }
                         } else {
                             console.log(`No {type} are available`);
                         }
                     }
                 })
+                return wd_data;
             } else {
                 console.log(`No ${type} available`);
+                return [];
             }
         } catch (err) {
             console.log("Error ", err);
@@ -69,10 +83,10 @@ let self = module.exports = {
             if (!word) {
                 word = await dict_hlpr.getRandomWord();
             }
-            console.log("Word is", word);
             // Display Word Definitions, Word Synonyms, Word Antonyms & Word Examples for a given word
             if (word) {
-                await Promise.all([self.defnOrexmple('definitions', word), self.synOrant('synonym', word), self.synOrant('antonym', word), self.defnOrexmple('examples', word)]);
+                console.log(`**Full Dictionary Data of the ${word}**\n`);
+                await Promise.all([self.defnOrexmple('definitions', word, false, true), self.synOrant('synonym', word, false, true), self.synOrant('antonym', word, false, true), self.defnOrexmple('examples', word, false, true)]);
                 process.exit();
             }
         } catch (err) {
@@ -85,31 +99,83 @@ let self = module.exports = {
     //word game
     //pending 
     playGame: async () => {
-        //         The command should display a definition, a synonym or an antonym and ask the user to guess the word. 
 
-        // Rules:
+        let word = await dict_hlpr.getRandomWord();
+        let result = {
+            data: [],
+            type: ''
+        };
+        let flag;
 
-        // - If the correct word is entered, show success message
-        // - Any synonyms of the word(expected answer) should be also be accepted as a correct answer.
-        // - If incorrect word is entered, user should be given 3 choices:
-        //     - (1) Try again
-        //         Let the user try again.
-        //     - (2) Hint
-        //         Display a hint, and let the user try again. Hints could be:
-        //             1. Display the word randomly jumbled (cat => atc, tac, tca)
-        //             2. Display another definition of the word
-        //             3. Display another antonym of the word
-        //             4. Display another synonym of the word
-        //     - (3) Quit
+        while (!result['data'].length) {
+            result = await self.gameWord(word);
+        }
+        // console.log("result", result);
 
-        //            Display the Word, Word Full Dict , and quit.
 
+
+        if (result) {
+            let guess_rand = Math.floor(Math.random() * (result.data.length));
+            console.log(`please guess the word for the given ${result.type}`);
+            console.log(`\n ${result.data[guess_rand]}`);
+            let solutions = [];
+            if (result.type !== 'synonym') {
+                solutions = await self.synOrant("synonym", word);
+            } else {
+                solutions = result.data;
+            }
+            let random_syn = Math.floor(Math.random() * (solutions.length));
+            let correct_ans = solutions[random_syn];
+            // console.log("Correct Answer is ", correct_ans);
+
+
+            rl.on('line', async (inp) => {
+                if (inp == correct_ans) {
+                    console.log("Success");
+                    rl.close()
+                } else {
+                    let opt = inp;
+                    if (opt == '1' && flag) {
+                        console.log(`\nEnter word\n`);
+                        flag = false;
+                    }
+                    else if (opt == '2' && flag) {
+                        let random_hint = Math.floor(Math.random() * (solutions.length));
+                        random_hint = (random_hint == random_syn) ? random_hint - 1 : random_hint;
+                        console.log(`Hint is ${solutions[random_hint]}`);
+                        flag = false;
+                    }
+                    else if (opt == '3' && flag) {
+                        console.log(`correct answer is ${correct_ans}\n`);
+                        await self.fullDict(word)
+                        flag = false;
+                        rl.close()
+                    }
+                    else {
+                        console.log(`Wrong Answer,Pick any of the given choices: \n 1.Try again \n 2.Hint \n 3.Quit the game\n`);
+                        flag = true;
+                    }
+                }
+            })
+
+
+        }
+    },
+
+    gameWord: async (word) => {
+        let random = Math.floor(Math.random() * (3));
+        let result = [];
+        let type = ["definition", "synonym", "antonym"];
+        if (random == 0) {
+            result = await self.defnOrexmple("definitions", word);
+        } else if (random == 1) {
+            result = await self.synOrant("synonym", word);
+        } else {
+            result = await self.synOrant("antonym", word);
+        }
+
+        return { data: result, type: type[random] };
     }
-
-
-
-
 }
-
 
 
--- dict.js
+++ dict.js
@@ -18,28 +18,31 @@ if (dict_args.length) {
                 console.log('Please enter the word to get definitions');
                 process.exit();
             }
-            dictionary.defnOrexmple('definitions', word, true);
+            dictionary.defnOrexmple('definitions', word, true, true);
             break;
         case 'ex':
             if (!word) {
                 console.log('Please enter the word to get examples');
                 process.exit();
             }
-            dictionary.defnOrexmple('examples', word, true);
+            dictionary.defnOrexmple('examples', word, true, true);
             break;
         case 'syn':
             if (!word) {
                 console.log('Please enter the word to get synonyms');
                 process.exit();
             }
-            dictionary.synOrant('synonym', word, true);
+            dictionary.synOrant('synonym', word, true, true);
             break;
         case 'ant':
             if (!word) {
                 console.log('Please enter the word to get antonyms');
                 process.exit();
             }
-            dictionary.synOrant('antonym', word, true);
+            dictionary.synOrant('antonym', word, true, true);
+            break;
+        case 'play':
+            dictionary.playGame();
             break;
         default:
             // key is the word because we are not preceeded with any of the type
